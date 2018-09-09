90dd7cb50a85ec2ba7461b9c6e95b279b88845da
Zack Lalanne <zack.lalanne@gmail.com>
2018-09-09 08:21:59 -0500

    Adding the config template for users

---
+++ src/config_template.h
@@ -0,0 +1,15 @@
+#define SSID                          "<SSID>"
+#define PASSWORD                      "<PASSWORD>"
+
+// MQTT Settings
+#define HOSTNAME                      "esp8266-master-bed"
+#define MQTT_SERVER                   "<MQTT_SERVER>"
+#define STATE_TOPIC                   "home/bedroom/bed"
+#define AVAILABILITY_TOPIC            "home/bedroom/bed/available"
+
+// OTA Settings
+#define OTA_PORT                      8266
+
+// Pin Settings
+#define HX711_DT                      5
+#define HX711_SCK                     4
